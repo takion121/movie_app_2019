49321818d276e0ef2a27cbfdd6cfe30ceaf1507d
안희태 <takion121@gmail.com>
2019-10-24 23:47:52 +0900

    class Component Test

--- src/App.js
+++ src/App.js
@@ -1,66 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "https://woochonfood.com/wp-content/uploads/2019/05/%EB%B0%B0%EC%B6%94%EA%B9%80%EC%B9%98-Napa-Cabbage-Kimchi.jpg",
-    rating: 7.2
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg",
-    rating: 9.4
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://recipe1.ezmember.co.kr/cache/recipe/2017/04/13/abcf293fb7d0d73d61e274127ced7b931.jpg",
-    rating: 5.7
-  },
-  {
-    id: 4,
-    name: "Kimchi",
-    image:
-      "https://woochonfood.com/wp-content/uploads/2019/05/%EB%B0%B0%EC%B6%94%EA%B9%80%EC%B9%98-Napa-Cabbage-Kimchi.jpg",
-    rating: 10.0
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Hello!");
   }
-];
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h4>{rating} / 10.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+  state = {
+    count: 0
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+  render() {
+    console.log("I'm rendering.");
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    console.log("component rendered.");
+  }
+
+  componentDidUpdate() {
+    console.log("I just updated!");
+  }
+
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+  }
 }
 
 export default App;
